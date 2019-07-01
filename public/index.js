ca0c2fffaaf00ea01cfa2d301c15084b930be3b5
Rachel Hague <r.m.hague@gmail.com>
2019-07-01 14:26:38 -0400

    Initial commit

--- public/index.js
+++ public/index.js
@@ -16,7 +16,28 @@ var app = new Framework7({
 
 document.getElementById("magic").addEventListener("submit", (evt)=>{
     evt.preventDefault();
-    const aAnswers = ["no", "yes"];
+    const aAnswers = [
+      "It is certain.",
+      "It is decidedly so.",
+      "Without a doubt.",
+      "Yes - definitely.",
+      "You may rely on it.",
+      "As I see it, yes.",
+      "Most likely.",
+      "Outlook good.",
+      "Yes.",
+      "Signs point to yes.",
+      "Reply hazy, try again.",
+      "Ask again later.",
+      "Better not tell you now.",
+      "Cannot predict now.",
+      "Concentrate and ask again.",
+      "Don't count on it.",
+      "My reply is no.",
+      "My sources say no.",
+      "Outlook not so good.",
+      "Very doubtful."
+      ];
     const nAnswer = Math.floor(Math.random() * aAnswers.length);
     document.getElementById("answer").innerHTML = aAnswers[nAnswer];
 });
